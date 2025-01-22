7a807a7b1701646da063d7f1895099ed48b1fe84
Jongs453 <107074951+Jongs453@users.noreply.github.com>
2025-01-22 13:11:31 +0100

    Add debugger files

---
+++ src/debugger.h
@@ -0,0 +1,22 @@
+/*
+* TinyRiscV-Simulator 2024
+* ===========================
+*
+* Project: https://github.com/LordBlacky/TinyRiscV-Simulator
+*
+*/
+
+#ifndef DISPLAY_H_
+#define DISPLAY_H_
+
+#include<stdint.h>
+
+// DISPLAY INTERFACE
+
+void sendCommand (int32_t command);
+
+void createDisplay ();
+
+void deleteDisplay ();
+
+#endif
