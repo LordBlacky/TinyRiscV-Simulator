b3b6bffa85d3c641fc07c2b4b17ac0b6b9708213
LordBlacky <93870579+LordBlacky@users.noreply.github.com>
2025-01-23 19:07:30 +0100

    Change lw, sw argument order in simulator

--- src/tinyriscvsimulator.c
+++ src/tinyriscvsimulator.c
@@ -359,11 +359,11 @@ void executeCommand (Command cmd, Register *reg, Memory *mem, Program *pgrm) {
 			pgrm->pc += 4;
 			break;
 		case LW:
-			wR(reg,rd,(rM(mem,rR(reg,rs1) + rs2)));
+			wR(reg,rd,(rM(mem,rR(reg,rs2) + rs1)));
 			pgrm->pc += 4;
 			break;
 		case SW:
-			wM(mem,rR(reg,rs1) + rs2,rR(reg,rd));
+			wM(mem,rR(reg,rs2) + rs1,rR(reg,rd));
 			pgrm->pc += 4;
 			break;
 		case BEQ:
